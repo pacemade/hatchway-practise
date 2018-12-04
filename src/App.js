251a7ed97aec0ab34e6dc98864d52972cb62e5ee
Danny Chang <dannychachang@gmail.com>
2018-12-04 11:19:28 -0500

    oh geez, managed to figure out that this within the .then needed to be an arrow function as well

--- src/App.js
+++ src/App.js
@@ -5,37 +5,55 @@ import './App.css';
 
 class App extends Component {
 
+  state = {
+    pokemon: [1,2,3]
+  }
 
-  // createNode = (e) => {
-  //   const node = document.createElement("li");
-  //   const textNode = document.createTextNode("Fifth");
-  //   node.appendChild(textNode);
-  //   document.getElementById("listy").appendChild(node);
-  // }
+  clearPokemonHandler = () => {
+    this.setState({ pokemon: [] })
+  }
+
+  pokemanArrayHandler = (list) => {
+    this.setState({ pokemon: list })
+  }
+
+  makeListHandler = (data) => {
+    console.log("is it working");
+    // for (var i = 0; i < data.results.length; i++) {
+    //   const node = document.createElement("li");
+    //   let textNode = document.createTextNode(data.results[i].name);
+    //   node.appendChild(textNode);
+    //   document.getElementById("listy").appendChild(node);
+    // }
+  }
 
   callTheApi = (e) => {
-    console.log("okay so far so good");
     fetch('https://pokeapi.co/api/v2/pokemon/')
       .then(function(response) {
         return response.json();
       })
-      .then(function(data) {
-        console.log(data);
-        for (var i = 0; i < data.results.length; i++) {
-          const node = document.createElement("li");
-          let textNode = document.createTextNode(data.results[i].name);
-          node.appendChild(textNode);
-          document.getElementById("listy").appendChild(node);
-        }
+      .then(data => {
+        this.makeListHandler(data);
       })
+  }
+
+  pleaseWork = (thing) => {
+    console.log("first work");
+    this.pleaseWorkAgain();
+  }
+
+  pleaseWorkAgain = () => {
+    console.log("second work");
   }
 
 
   render() {
     return (
       <div>
-        <h1> Oh damn this was harder than I thought for reals </h1>
-        <button onClick={(e) => this.callTheApi(e)}>Throw me information</button>
+        <h1> {this.state.pokemon} </h1>
+        <button onClick={this.callTheApi}>Throw me information</button>
+        <button onClick={this.pleaseWork}>please work</button>
+        <button onClick={this.clearPokemonHandler}>Clear The array</button>
         <Filter />
         <ul id="listy">
           <li>First</li>
--- src/components/Filters/Filter.js
+++ src/components/Filters/Filter.js
@@ -3,12 +3,31 @@ import React, { Component } from 'react';
 class Filter extends Component {
 
   state = {
-    pokemon: ''
+    pokemon2: ["silly", "ppopoo"]
+  }
+
+  getPokemonHandler = (e) => {
+    console.log("okay so far so good");
+    fetch('https://pokeapi.co/api/v2/pokemon/')
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(data) {
+        console.log(data);
+        for (var i = 0; i < data.results.length; i++) {
+          const node = document.createElement("li");
+          let textNode = document.createTextNode(data.results[i].name);
+          node.appendChild(textNode);
+          document.getElementById("listy").appendChild(node);
+        }
+      })
   }
 
   render() {
     return(
-      <h1>does this still work</h1>
+      <div>
+        <h1>{this.state.pokemon2}</h1>
+      </div>
     )
   }
 }
